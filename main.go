e0d0e2c84e36f2181f1431a761e1a8eb93633d5e
John Jeffery <jjeffery@sp.com.au>
2016-07-22 08:17:35 +1000

    Remove unused code

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"html/template"
 	"io"
 	"log"
 	"os"
-	"regexp"
 	"strconv"
-	"strings"
 
 	"github.com/jjeffery/vt-motoli/scanner"
 	"github.com/jjeffery/vt-motoli/story"
@@ -28,34 +25,19 @@ func main() {
 	}
 
 	s := scanStory(file)
-
 	printStory(s)
 }
-
-// Regular expressions for parsing the line contents
-var (
-	// regular expressions indicating a comment line
-	commentREs = []*regexp.Regexp{
-		regexp.MustCompile(`^#(#|[^#]+\s)`), // eg "### comment"
-		regexp.MustCompile(`^#[^#]\s`),      // eg "#This is a comment"
-		regexp.MustCompile(`^\s*<`),         // eg "<Identifier> <-- start of line etc"
-	}
-
-	commandRE = regexp.MustCompile(`^#([A-Za-z0-9-]+)#\s+(.*)$`)
-)
 
 func scanStory(r io.Reader) *story.Story {
 	s := story.New()
 	scan := scanner.New(r)
 	for scan.Scan() {
-		if scan.Err != nil {
-			log.Fatal(scan.Err)
-		}
 		if scan.Prefixes.Match("Page", "Line") {
 			pageNum := scan.Prefixes[0].Index
 			lineNum := scan.Prefixes[1].Index
+			continuationNum := scan.Prefixes[1].Cont
 			line := s.Page(pageNum).Line(lineNum)
-			line.SetText(scan.Prefixes[1].Cont, scan.Arg)
+			line.SetText(continuationNum, scan.Arg)
 		} else if scan.Prefixes.Match("Page", "Text") {
 			pageNum := scan.Prefixes[0].Index
 			textNum := scan.Prefixes[1].Index
@@ -82,48 +64,11 @@ func scanStory(r io.Reader) *story.Story {
 			}
 		}
 	}
-	return s
-}
-
-func scanStory_1(r io.Reader) *story.Story {
-	s := story.New()
-	scanner := bufio.NewScanner(r)
-
-	for line := 1; scanner.Scan(); line++ {
-		text := scanner.Text()
-		text = strings.TrimSpace(text)
-		if text == "" {
-			debug.Printf("line %d: blank\n", line)
-			continue
-		}
-
-		if isComment(text) {
-			debug.Printf("line %d: comment\n", line)
-			continue
-		}
-
-		if cmd := commandRE.FindStringSubmatch(text); len(cmd) > 0 {
-			debug.Printf("line %d: %q %q\n", line, cmd[1], cmd[2])
-			if err := s.Process(cmd[1], cmd[2]); err != nil {
-				log.Fatal("line %d: %v", line, err)
-			}
-			continue
-		}
-
-		// TODO(jpj):
-		log.Fatal("line %d: unknown: %q\n", line, text)
+	if scan.Err != nil {
+		log.Fatal(scan.Err)
 	}
 
 	return s
-}
-
-func isComment(text string) bool {
-	for _, re := range commentREs {
-		if re.MatchString(text) {
-			return true
-		}
-	}
-	return false
 }
 
 func printStory(s *story.Story) {
@@ -131,6 +76,9 @@ func printStory(s *story.Story) {
 	t.Execute(os.Stdout, s)
 }
 
+// TODO(jpj): This is just an example of formatting the HTML based
+// on the story data model. Need to examine the existing script and
+// update tmpl accordingly.
 const tmpl = `<!doctype html>
 <html>
 <head>
--- story/line.go
+++ story/line.go
@@ -1,29 +1,18 @@
 package story
 
-import (
-	"fmt"
-)
-
 type Line struct {
 	Number int
-	Texts  []string
+	Texts  map[int]string
 	Time   float64
 }
 
 func newLine(num int) *Line {
 	return &Line{
 		Number: num,
+		Texts:  make(map[int]string),
 	}
 }
 
-func (line *Line) SetText(num int, text string) error {
-	if len(line.Texts) == num {
-		line.Texts = append(line.Texts, text)
-		return nil
-	}
-	if len(line.Texts) > num {
-		line.Texts[num] = text
-		return nil
-	}
-	return fmt.Errorf("invalid index: %d", num+1)
+func (line *Line) SetText(num int, text string) {
+	line.Texts[num] = text
 }
